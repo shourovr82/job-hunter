745c56393f02720f939f87f081a5d52cada40c46
Aminul Islam Rakib <rakib38324@gmail.com>
2023-01-10 20:41:13 +0600

    Background yellow

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const index = () => {
   return (
     <section>
-      <div className="shadow w-full  h-screen bg-red-400">
+      <div className="shadow w-full  h-screen bg-yellow-400">
         <h1 className="text-center">this is home page</h1>
         <h1 className="text-center">From Aminul Islam Rakib</h1>
         <h2>from shafinur islam</h2>
