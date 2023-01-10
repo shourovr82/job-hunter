e935a6958d6c56697f5e6042cfce927e1f0045ac
Tahdi Islam <tshdi60@gmail.com>
2023-01-10 20:27:09 +0600

    m

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,10 @@ import React from "react";
 const index = () => {
   return (
     <section>
-      <div className="shadow w-full  h-screen bg-green-400">
+      <div className="shadow w-full  h-screen bg-red-400">
         <h1 className="text-center">this is home page</h1>
         <h1 className="text-center">From Aminul Islam Rakib</h1>
+        <h1 className="text-center">From Tahdi Islam</h1>
       </div>
     </section>
   );
