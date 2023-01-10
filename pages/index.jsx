df7cf0eb2c66629343c9cd3d39c740077e860729
Aminul Islam Rakib <rakib38324@gmail.com>
2023-01-10 20:17:05 +0600

    Commit a sentence

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,7 @@ const index = () => {
     <section>
       <div className="shadow w-full  h-screen bg-green-400">
         <h1 className="text-center">this is home page</h1>
+        <h1 className="text-center">From Aminul Islam Rakib</h1>
       </div>
     </section>
   );
