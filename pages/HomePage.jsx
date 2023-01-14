b2a2b53e7ee9eb1f7b37c46a3524a7f4656639a0
shourovr82 <shafinur512@gmail.com>
2023-01-14 23:13:29 +0600

    added icon and some design changed on add post component

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -2,38 +2,42 @@ import React from "react";
 import { BsSave2Fill, BsCurrencyDollar, BsPeopleFill } from "react-icons/bs";
 import Root from "../utility/Root";
 import CreateSocialPost from "./createSocialPost";
+import { RiVideoAddFill } from 'react-icons/ri';
+import { FaPhotoVideo } from 'react-icons/fa';
+import { BsEmojiLaughing } from 'react-icons/bs';
+
 
 const HomePage = () => {
   return (
     <section>
       <Root>
-        <div className="shadow bg-white w-full rounded p-10 mb-5">
+        <div className="shadow  bg-white w-full rounded-lg p-4 mb-5">
           <div className="flex items-center justify-start gap-6 border-b pb-6">
             <div>
               <img
-                className="w-20 h-20 rounded-full"
+                className="w-14 h-14 rounded-full"
                 src="https://i.ibb.co/QYjRJmD/avator.jpg"
                 alt="brand logo"
               />
             </div>
-            <div className="w-4/5">
+            <div className="w-full">
               <label
                 htmlFor="social-post"
-                className="btn btn-block justify-start normal-case rounded-full border-0 shadow-xl no-animation"
+                className="btn w-full  border-0 justify-start bg-[#ebeef3] hover:bg-[#d5dae0] normal-case text-black font-semibold  rounded-full  no-animation"
               >
-                What's on your mind?
+                {`What's on your mind?`}
               </label>
             </div>
           </div>
-          <div className="flex flex-wrap justify-between gap-4 pt-6 text-lg">
-            <a className=" bg-white py-2 px-6 rounded-lg btn border-0">
-              Live video
+          <div className="flex flex-wrap justify-between gap-4 mt-4">
+            <a className="flex hover:bg-[#ebecec] cursor-pointer items-center  gap-3 bg-white py-2 px-6 rounded-lg text-sm font-bold ">
+              <span> <RiVideoAddFill className="text-2xl  text-red-600" /></span> Live video
             </a>
-            <a className=" bg-white py-2 px-6 rounded-lg btn border-0">
-              Photo/video
+            <a className="bg-white hover:bg-[#ebecec] cursor-pointer flex items-center gap-3 py-2 px-6 rounded-lg  text-sm font-bold">
+              <span> <FaPhotoVideo className="text-2xl  text-green-600" /></span> Photo/video
             </a>
-            <a className=" bg-white py-2 px-6 rounded-lg btn border-0">
-              feeling/activity
+            <a className="bg-white hover:bg-[#ebecec] cursor-pointer flex items-center gap-3 py-2 px-6 rounded-lg  text-sm font-bold">
+              <span> <BsEmojiLaughing className="text-2xl  text-yellow-500" /></span>Feeling/Activity
             </a>
           </div>
         </div>
